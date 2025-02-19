642bc635075c95fdcbe5ac32c7d9b6d56e5a8a92
lijian <ljzhuanjiao@gmail.com>
2025-02-19 11:54:56 +0800

    package

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,6 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import './style.css';
+import 'element-plus/dist/index.css' // 引入 Element Plus 样式
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.mount('#app')
